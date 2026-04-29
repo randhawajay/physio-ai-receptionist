a90865811ccbb26b31b2e5d91a9569e4fa293f5c
randhawajay <randhawajay2468@gmail.com>
2026-04-28 20:16:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+import express from "express";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.post("/call", (req, res) => {
   console.log("TWILIO HIT /call");
 
   res.type("text/xml");
-  res.send(`
+  res.send(twiml);
 <Response>
   <Say>Hello. Twilio is connected to Render successfully.</Say>
 </Response>
 `);
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
 });
